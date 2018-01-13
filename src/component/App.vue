7e87e96d8fbfcd4dae32dd8d6e323f35658327dc
spj12138 <spj12138@163.com>
2018-01-13 14:11:40 +0800

    亂來的

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,43 +1,24 @@
 <template>
-  <div> {{msg}}world
+  <div> 
+    
+
+
     <router-view></router-view>
-    <el-button type="text" @click="open2">点击打开</el-button>
+   
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        msg: '亂來的'
-      }
-    },
-    methods: {
-      open2() {
-        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          });
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          });
-        });
-      }
-    }
+ 
+   
   }
 </script>
 
 
 <style>
-  div {
+  /* div {
     color: rgb(0, 255, 42);
     font-size: 40PX;
-  }
+  } */
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ElementUI from "element-ui";
 import 'element-ui/lib/theme-chalk/index.css';
-import Normalize from 'normalize';
+import 'normalize.css';
 
 
 //啟動vue插件
